bdc1f10489b4daad18a46289e83925a01ccd3424
NazrawitGemechu <nazrawitgemechu9706@gmail.com>
2023-06-29 10:27:49 +0300

    updated

---
+++ TalentManagement.Application/Commands/CommandHandler/CreateTalentHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentManagement.Application.Commands.TalentCommand;
+using TalentManagement.Domain.Entities;
+using TalentManagement.Persistance.Data;
+
+namespace TalentManagement.Application.Commands.CommandHandler
+{
+    public class CreateTalentHandler : IRequestHandler<CreateTalentCommand, Talent>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CreateTalentHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Talent> Handle(CreateTalentCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TalentManagement.Application/Commands/TalentCommand/CreateTalentCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TalentManagement.Domain.Entities;
+
+namespace TalentManagement.Application.Commands.TalentCommand
+{
+    public class CreateTalentCommand : IRequest<Talent>
+    {
+        public Talent NewTalent { get; set; }
+    }
+}
